865760fef961ba481cecb3ad575c22bf9b357355
Norluciot <norluciot.adrien@gmail.com>
2024-07-10 13:13:43 +0300

    line done

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -9,7 +9,7 @@ const TodoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
     <div className='flex items-center my-3 gap-2'>
       <div onClick={()=>{toggle(id)}} className="flex flex-1 items-center cursor-pointer ">
         <img src={isComplete ? tick : not_tick} alt="" className='w-7' />
-        <p className='text-slate-700 ml-4 text-[17px}' >{text}</p>
+        <p className={`text-slate-700 ml-4 text-[17px} decoration-state-500 ${isComplete ? "line-through" : ""}`} >{text}</p>
       </div>
 
     <img onClick={()=>{deleteTodo(id)}} src={delete_icon} alt="" className='w-3.5 cursor-pointer'/>
